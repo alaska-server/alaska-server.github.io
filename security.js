92d2fbd4734fb236ee33fb5b205397f09b7b2e47
alaska-server <alaska.unknown.404@gmail.com>
2024-08-08 21:38:48 -0400

    Add files via upload

--- security.js
+++ security.js
@@ -1,10 +1,10 @@
 function checkPassword() {
     const passwordInput = document.getElementById("password");
-    const correctPassword = "B20A10P";
+    const correctPassword = "GRAFFITI";
     const linkContainer = document.getElementById("linkContainer");
 
     if (passwordInput.value === correctPassword) {
-      linkContainer.innerHTML = '<a href="security.html">Correct Password.<br>Click To Security Two.</a>';
+      linkContainer.innerHTML = '<a href="Log.html">Correct Password.<br>Click To Access.</a>';
     } else {
       linkContainer.innerHTML = 'Incorrect Pasword.';
     }
